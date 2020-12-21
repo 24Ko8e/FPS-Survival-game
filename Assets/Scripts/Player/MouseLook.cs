b2db901184dbea7d1aa76cdd6dd532cd78865aa8
Abhishek <abhishekm942@gmail.com>
2020-12-22 01:16:59 +0530

    Added movement and cursor lock

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] Transform playerRoot, lookAroundRoot;
+    [SerializeField] bool invert;
+    [SerializeField] bool canUnlock = true;
+    [SerializeField] float sensitivity = 5f;
+    [SerializeField] int smoothSteps = 10;
+    [SerializeField] float smoothWeight = 0.4f;
+    [SerializeField] float rollAngle = 10f;
+    [SerializeField] Vector2 lookLimits = new Vector2(-90f, 90f);
+    Vector2 lookAngles;
+    Vector2 currentMouseLook;
+    Vector2 smoothMove;
+    float currentRollAngle;
+    int lastLookFrame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cursorLockToggle();
+    }
+
+    void cursorLockToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+
+    void LookAround()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController characterController;
+    Vector3 move_direction;
+    public float speed = 5f;
+    float gravity = 20f;
+    public float jump_force = 10f;
+    float vertical_velocity;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        move_direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        move_direction = transform.TransformDirection(move_direction);
+        move_direction *= speed * Time.deltaTime;
+
+        ApplyGravity();
+        characterController.Move(move_direction);
+    }
+
+    private void ApplyGravity()
+    {
+        vertical_velocity -= gravity * Time.deltaTime;
+        PlayerJump();
+
+        move_direction.y = vertical_velocity * Time.deltaTime;
+    }
+
+    private void PlayerJump()
+    {
+        if (characterController.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            vertical_velocity = jump_force;
+        }
+    }
+}
