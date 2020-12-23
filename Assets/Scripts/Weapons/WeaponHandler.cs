206cb546a57573ac5738d49c887b45312f4205ea
Abhishek <abhishekm942@gmail.com>
2020-12-23 20:32:32 +0530

    Updated weapon handler script

--- Assets/Scripts/Weapons/WeaponHandler.cs
+++ Assets/Scripts/Weapons/WeaponHandler.cs
@@ -2,17 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum weaponAim
+{
+    NONE,
+    SELF_AIM,
+    AIM
+}
+
+public enum weaponFireType
+{
+    SINGLE,
+    MULTIPLE
+}
+
+public enum weaponBulletType
+{
+    BULLET,
+    ARROW,
+    SPEAR,
+    NONE
+}
+
 public class WeaponHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator anim;
+    public weaponAim weapon_Aim;
+
+    [SerializeField]
+    GameObject muzzleFlash;
+
+    [SerializeField]
+    AudioSource shootSound, reloadSound;
+    public weaponFireType fireType;
+    public weaponBulletType bulletType;
+
+    public GameObject attackPoint;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void shootAnimation()
+    {
+        anim.SetTrigger("Shoot");
+    }
+
+    public void Aim(bool canAim)
+    {
+        anim.SetBool("Aim", canAim);
+    }
+
+    void turnOnMuzzleFlash()
+    {
+        muzzleFlash.SetActive(true);
+    }
+
+    void turnOffMuzzleFlash()
+    {
+        muzzleFlash.SetActive(false);
+    }
+
+    void playShootSound()
+    {
+        shootSound.Play();
+    }
+
+    void playReloadSound()
+    {
+        reloadSound.Play();
+    }
+
+    void turnOnAttackPoint()
+    {
+        attackPoint.SetActive(true);
+    }
+
+    void turnOffAttackPoint()
+    {
+        if(attackPoint.activeInHierarchy)
+            attackPoint.SetActive(false);
     }
 }
