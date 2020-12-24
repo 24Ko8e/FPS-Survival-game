459a7249d07cb1371aa1ee24118ee2990a79d54e
Abhishek <abhishekm942@gmail.com>
2020-12-24 21:55:07 +0530

    Added attacking and shooting, fixed gun animations and added gun ADS zooming

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    WeaponManager weaponManager;
+    public float fireRate = 15f;
+    float nxtTimeToFire;
+    public float damage = 20f;
+
+    public Animator camZoomAnimator;
+    bool isZoomed;
+    Camera mainCam;
+    public GameObject crosshair;
+
+    void Awake()
+    {
+        weaponManager = GetComponent<WeaponManager>();
+        mainCam = Camera.main;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        WeaponShoot();
+        ZoomInAndOut();
+    }
+
+    void WeaponShoot()
+    {
+        if (weaponManager.getSelectedWeapon().fireType == weaponFireType.MULTIPLE)
+        {
+            if (Input.GetMouseButton(0) && Time.time > nxtTimeToFire)
+            {
+                nxtTimeToFire = Time.time + 1f / fireRate;
+                weaponManager.getSelectedWeapon().shootAnimation();
+                BullerFired();
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (weaponManager.getSelectedWeapon().tag == "Axe")
+                {
+                    weaponManager.getSelectedWeapon().shootAnimation();
+                }
+                if (weaponManager.getSelectedWeapon().bulletType == weaponBulletType.BULLET)
+                {
+                    weaponManager.getSelectedWeapon().shootAnimation();
+                    BullerFired();
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+
+    private void BullerFired()
+    {
+        
+    }
+
+    private void ZoomInAndOut()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField]
+    WeaponHandler[] weapons;
+
+    int currentWeaponIndex;
+
+    void Start()
+    {
+        currentWeaponIndex = 0;
+        weapons[currentWeaponIndex].gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            setSelectedWeapon(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            setSelectedWeapon(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            setSelectedWeapon(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            setSelectedWeapon(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            setSelectedWeapon(4);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            setSelectedWeapon(5);
+        }
+    }
+
+    void setSelectedWeapon(int weaponIndex)
+    {
+        if (currentWeaponIndex == weaponIndex)
+            return;
+
+        weapons[currentWeaponIndex].gameObject.SetActive(false);
+
+        currentWeaponIndex = weaponIndex;
+        weapons[currentWeaponIndex].gameObject.SetActive(true);
+    }
+
+    public WeaponHandler getSelectedWeapon()
+    {
+        return weapons[currentWeaponIndex];
+    }
+}
