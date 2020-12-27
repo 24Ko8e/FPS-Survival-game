26b2e2c4f2a2c9312a2dd5e4db0e33783032ce61
Abhishek <abhishekm942@gmail.com>
2020-12-27 22:25:46 +0530

    Setup animator controller

---
+++ Assets/Scripts/Enemy Scripts/EnemyAnimator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimator : MonoBehaviour
+{
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Walk(bool walk)
+    {
+        anim.SetBool("Walk", walk);
+    }
+
+    public void Run(bool run)
+    {
+        anim.SetBool("Run", run);
+    }
+
+    public void Attack()
+    {
+        anim.SetTrigger("Attack");
+    }
+
+    public void Dead()
+    {
+        anim.SetTrigger("Dead");
+    }
+}
---
+++ Assets/Scripts/Enemy Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public enum EnemyState{
+    PATROL,
+    CHASE,
+    ATTACK
+}
+
+public class EnemyController : MonoBehaviour
+{
+    EnemyAnimator enemyAnim;
+    NavMeshAgent navAgent;
+    EnemyState enemyState;
+
+    public float walkSpeed = 0.5f;
+    public float runSpeed = 4f;
+    public float chaseDistance = 7f;
+    float currentChaseDist;
+    public float attackDist = 1.8f;
+    public float chaseAfterAttackDistance = 2f;
+    public float patrolRadiusMin = 20f;
+    public float patrolRadiusMax = 60f;
+    public float patrolDuration = 15f;
+    float patrolTimer;
+    public float waitBeforeAttack = 2f;
+    float attackTimer;
+
+    Transform target;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
