22c8098ed8cbb760dda69d8bdc97cb8494540f68
Abhishek <abhishekm942@gmail.com>
2020-12-30 18:10:07 +0530

    Added health script

---
+++ Assets/Scripts/Player/HealthScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HealthScript : MonoBehaviour
+{
+    EnemyAnimator enemyAnim;
+    NavMeshAgent navAgent;
+    EnemyController enemyController;
+
+    public float health = 100f;
+    public bool isPlayer, isBoar, isCannibal;
+    bool isDead;
+
+    private void Awake()
+    {
+        if (isBoar || isCannibal)
+        {
+
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
