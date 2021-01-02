236070537e3387f1b7452a9fc8d88927a566db7c
Abhishek <abhishekm942@gmail.com>
2021-01-02 14:22:52 +0530

    Added sound effects

---
+++ Assets/Scripts/Player/AxeSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeSound : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+    [SerializeField]
+    AudioClip[] audioClips;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void PlaySound()
+    {
+        audioSource.clip = audioClips[UnityEngine.Random.Range(0, audioClips.Length)];
+        audioSource.Play();
+    }
+}
